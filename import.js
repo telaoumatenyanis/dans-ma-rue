9aa6a60a95722e2f0edbec1953d6b991100dd4db
yanis.telaoumaten <yanis.telaoumaten@sterblue.com>
2020-02-17 13:36:25 +0100

    feat: add the ability to run the import script with the given data

--- import.js
+++ import.js
@@ -1,28 +1,103 @@
-const config = require('config');
-const csv = require('csv-parser');
-const fs = require('fs');
-const { Client } = require('@elastic/elasticsearch');
-const indexName = config.get('elasticsearch.index_name');
+const config = require("config");
+const csv = require("csv-parser");
+const fs = require("fs");
+const { Client } = require("@elastic/elasticsearch");
+const moment = require("moment");
+const { chunk } = require("lodash/fp");
+const bluebird = require("bluebird");
 
-async function run () {
-    // Create Elasticsearch client
-    const client = new Client({ node: config.get('elasticsearch.uri') });
+const indexName = config.get("elasticsearch.index_name");
+const MAX_CHUNK_SIZE = 10000;
 
-    // TODO il y a peut être des choses à faire ici avant de commencer ... 
+async function run() {
+  // Create Elasticsearch client
+  const client = new Client({ node: config.get("elasticsearch.uri") });
 
-    // Read CSV file
-    fs.createReadStream('dataset/dans-ma-rue.csv')
-        .pipe(csv({
-            separator: ';'
-        }))
-        .on('data', (data) => {
-            // TODO ici on récupère les lignes du CSV ...
-            console.log(data);
-        })
-        .on('end', () => {
-            // TODO il y a peut être des choses à faire à la fin aussi ?
-            console.log('Terminated!');
-        });
+  // Index creation
+  try {
+    if (!(await checkIfIndexExists(client, indexName))) {
+      await client.indices.create({ index: indexName });
+      console.log("Created index 'anomalies'");
+    } else {
+      console.log("Index already exists, skipping index creation");
+    }
+  } catch (err) {
+    console.trace(err.message);
+  }
+
+  const anomalies = [];
+  let i = 0;
+  // Read CSV file
+  fs.createReadStream("dataset/dans-ma-rue.csv")
+    .pipe(
+      csv({
+        separator: ";"
+      })
+    )
+    .on("data", data => {
+      // Push current data
+      anomalies.push({
+        "@timestamp": data.DATEDECL,
+        object_id: data.OBJECTID,
+        annee_declaration: data["ANNEE DECLARATION"],
+        mois_declaration: data["MOIS DECLARATION"],
+        type: data.TYPE,
+        sous_type: data.SOUSTYPE,
+        code_postal: data.CODE_POSTAL,
+        ville: data.VILLE,
+        arrondissement: data.ARRONDISSEMENT,
+        prefixe: data.PREFIXE,
+        intervenant: data.INTERVENANT,
+        conseil_de_quartier: data["CONSEIL DE QUARTIER"],
+        location: data.geo_point_2d
+      });
+    })
+    .on("end", async () => {
+      try {
+        /**
+         * It is needed to chunk the data in order to not exceed the elasticsearch query limit.
+         * In order to limit load on ES, maximum concurrent running bulk are 10
+         */
+        console.log("Inserting " + anomalies.length + " anomalies");
+        await bluebird.map(
+          chunk(MAX_CHUNK_SIZE, anomalies),
+          anomalyChunk => {
+            return client.bulk(createBulkInsertQuery(anomalyChunk));
+          },
+          { concurrency: 10 }
+        );
+      } catch (err) {
+        console.trace(err);
+      } finally {
+        client.close();
+      }
+    });
+}
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(anomalies) {
+  const body = anomalies.reduce((acc, anomaly) => {
+    const { object_id, ...params } = anomaly;
+    acc.push({
+      index: { _index: indexName, _type: "_doc", _id: object_id }
+    });
+    acc.push(params);
+    return acc;
+  }, []);
+
+  return { body };
+}
+
+/**
+ * Check if an index already exists
+ *
+ * @param   {any}     client      The elasticsearch client
+ * @param   {String}  indexName   The name of the index to check
+ *
+ * @return  {Boolean}             true if index already exists, false otherwise
+ */
+async function checkIfIndexExists(client, indexName) {
+  return (await client.indices.exists({ index: indexName })).body;
 }
 
 run().catch(console.error);
--- services/count.js
+++ services/count.js
@@ -1,16 +1,15 @@
-const config = require('config');
-const indexName = config.get('elasticsearch.index_name');
+const config = require("config");
+const indexName = config.get("elasticsearch.index_name");
 
-exports.count = (client, from, to, callback) => {
-    // TODO Compter le nombre d'anomalies entre deux dates
-    callback({
-        count: 0
-    })
-}
+exports.count = async (client, from, to, callback) => {
+  callback({
+    count: 0
+  });
+};
 
 exports.countAround = (client, lat, lon, radius, callback) => {
-    // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
-    callback({
-        count: 0
-    })
-}
+  // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
+  callback({
+    count: 0
+  });
+};
